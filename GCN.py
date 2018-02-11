14ae3fe9dbe47ca1d55c812940575979d4554808
Sebastian Borgeaud <spb61@cl.cam.ac.uk>
2018-02-11 10:28:10 +0000

    Add dropout and fix pre_processing of adj matrix

--- GCN.py
+++ GCN.py
@@ -3,17 +3,13 @@ import scipy
 
 import tensorflow as tf 
 from tensorflow.contrib.layers import xavier_initializer
+from tensorflow import glorot_uniform_initializer
 
 from utils import load_data
 
 adj, features, y_train, y_val, y_test, train_mask, val_mask, test_mask = load_data('cora')
 
-print(adj.shape)
-print(y_train.shape)
-print(y_val.shape)
-print(y_test.shape)
-print(train_mask.shape)
-
+print("Total number of nodes: {}".format(adj.shape[0]))
 print("Training nodes: {}".format(len(np.argwhere(y_train == True))))
 print("Validation nodes: {}".format(len(np.argwhere(y_val == True))))
 print("Test nodes: {}".format(len(np.argwhere(y_test == True))))
@@ -23,42 +19,61 @@ num_features = features.shape[1]
 num_classes = y_train.shape[1]
 
 # Hyperparameters
-num_epochs = 50
-hidden_units = 1024
+num_epochs = 200
+hidden_units = 16
+initializer = glorot_uniform_initializer
+dropout = 0.5
+print_info_every = 20
 
 # preprocessing, compute A
 I = scipy.sparse.identity(num_nodes, format='csr')
 A_tilde = adj + I
 degrees = np.array(np.sum(A_tilde, axis=1))
-degrees_inv = np.power(degrees, -1).flatten()
-D_inv = scipy.sparse.diags(degrees_inv)
-A_hat = D_inv.multiply(A_tilde).multiply(D_inv)
+degrees_inv_sqrt = np.power(degrees, -0.5).flatten()
+D_inv_sqrt = scipy.sparse.diags(degrees_inv_sqrt)
+A_hat = D_inv_sqrt.dot(A_tilde).dot(D_inv_sqrt)
 A_hat = tf.constant(np.array(A_hat.todense()), dtype=tf.float32, name='A_hat')
 
 #TODO normalize features?
+def normalize_features(features):
+    """row-normalize features"""
+    row_sum = np.array(np.sum(features, axis=1))
+    r_inv = np.power(row_sum, -1).flatten()
+    r_inv[np.isinf(r_inv)] = 0
+    r_mat_inv = scipy.sparse.diags(r_inv)
+    features = r_mat_inv.dot(features)
+    return features
 
+features = normalize_features(features)
 # Create GCN
 X = tf.placeholder(dtype=tf.float32, shape=[None, num_features])
 y = tf.placeholder(dtype=tf.int32, shape=[None, num_classes])
 mask = tf.placeholder(dtype=tf.bool, shape=[num_nodes])
 
-W_0 = tf.get_variable("W_0", [num_features, hidden_units], dtype=tf.float32, initializer=xavier_initializer())
+dropout_rate = tf.placeholder_with_default(0.0, shape=(), name="dropout")
 
-Z_0 = tf.matmul(X, W_0)
+W_0 = tf.get_variable("W_0", [num_features, hidden_units], dtype=tf.float32, initializer=initializer())
+W_1 = tf.get_variable("W_1", shape=[hidden_units, num_classes], dtype=tf.float32, initializer=initializer())
+
+X_dropped = tf.nn.dropout(X, keep_prob = 1-dropout_rate)
+
+Z_0 = tf.matmul(X_dropped, W_0)
 Z_0 = tf.matmul(A_hat, Z_0)
 Z_0 = tf.nn.relu(Z_0)
 
-W_1 = tf.get_variable("W_1", shape=[hidden_units, num_classes], dtype=tf.float32, initializer=xavier_initializer())
 
-logits = tf.matmul(tf.matmul(A_hat, Z_0), W_1, name="logits")
+Z_0_dropped = tf.nn.dropout(Z_0, keep_prob = 1-dropout_rate)
+logits = tf.matmul(tf.matmul(A_hat, Z_0_dropped), W_1, name="logits")
 Z = tf.nn.softmax(logits=logits, name="probabilities")
 
 loss = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=tf.boolean_mask(logits, mask), labels=tf.boolean_mask(y, mask)))
 
-predictions = tf.argmax(Z, axis=-1)
+predictions = tf.boolean_mask(tf.argmax(Z, axis=-1), mask)
+labels = tf.boolean_mask(tf.argmax(y, axis=-1), mask)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predictions,  labels), dtype=tf.float32))
+accuracy2 = tf.metrics.accuracy(labels=tf.argmax(tf.boolean_mask(y,mask), axis=-1), predictions=tf.boolean_mask(predictions, mask))
 
-accuracy = tf.metrics.accuracy(labels=tf.argmax(tf.boolean_mask(y,mask), axis=-1), predictions=tf.boolean_mask(predictions, mask))
-optimizer = tf.train.AdamOptimizer().minimize(loss)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
 
 
 with tf.Session() as sess:
@@ -66,16 +81,16 @@ with tf.Session() as sess:
     sess.run(tf.local_variables_initializer())
     for epoch in range(num_epochs):
         _, loss_, accuracy_ = sess.run([optimizer, loss, accuracy], 
-                feed_dict = {X: np.array(features.todense()), y:y_train, mask: train_mask})
-        if epoch % 1 == 0:
-            print("Epoch:{:3d}\tLoss: {:.5f}\tAcc: {:.3f}".format(epoch, loss_, accuracy_[0]))
+                feed_dict = {X: np.array(features.todense()), y:y_train, mask: train_mask, dropout_rate: dropout})
+        if epoch % print_info_every == 0:
+            print("Epoch:{:3d}\tLoss: {:.5f}\tAcc: {:.5f}".format(epoch, loss_, accuracy_))
 
             val_accuracy_ = sess.run(accuracy, 
                     feed_dict = {X: np.array(features.todense()), y:y_val, mask: val_mask})
-            print("Validation accuracy: {:.3f}".format(val_accuracy_[0]))
+            print("Validation accuracy: {:.5f}".format(val_accuracy_))
 
 
-    test_accuracy_ = sess.run(accuracy, 
+    test_accuracy_, predictions_ = sess.run([accuracy, predictions],
             feed_dict = {X: np.array(features.todense()), y:y_test, mask: test_mask})
-    print("Test accuracy: {:.3f}".format(test_accuracy_[0]))
+    print("Test accuracy: {:.5f}".format(test_accuracy_))
 
